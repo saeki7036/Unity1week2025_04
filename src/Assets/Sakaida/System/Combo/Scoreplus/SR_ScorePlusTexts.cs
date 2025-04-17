9a14112173b260b6c285ba775a34a4d91c1433d2
Buff <g2227037@st.t-kougei.ac.jp>
2025-04-17 21:03:15 +0900

    もっといろいろ追加

---
+++ src/Assets/Sakaida/System/Combo/Scoreplus/SR_ScorePlusTexts.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SR_ScorePlusTexts : MonoBehaviour
+{
+
+    public TextMeshProUGUI Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
